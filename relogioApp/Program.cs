e2c8a22d532c654ded69e15c31aca561352e84a3
pedrofalchi-fullstack <peeh.falchi@gmail.com>
2026-08-29 04:13:45 -0300

    concluido

--- relogioApp/Program.cs
+++ relogioApp/Program.cs
@@ -3,20 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
-namespace relogioApp
+
+/*
+ Exercício 1 - Relógio Digital
+ Criar um relógio em C# que mostre a hora atual no formato
+ HH:MM:SS, atualizando a cada segundo.
+ Para finalizar o programa, o usuário deve pressionar
+ a tecla X ou x.
+
+ */
+
+
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        bool continuar = true;
+
+        Console.WriteLine("RELÓGIO DIGITAL");
+        Console.WriteLine("Pressione X para sair.");
+        Console.WriteLine();
+
+        while (continuar)
         {
-            Console.WriteLine("Relógio");
+            // Pega a hora atual
+            DateTime horaAtual = DateTime.Now;
 
+            // Mostra a hora na mesma linha
+            Console.Write("\r" + horaAtual.ToString("HH:mm:ss"));
 
+            // Verifica se alguma tecla foi pressionada
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo tecla = Console.ReadKey(true);
 
+                // Verifica se foi X ou x
+                if (tecla.Key == ConsoleKey.X)
+                {
+                    continuar = false;
+                }
+            }
+
+            // Aguarda 1 segundo
+            Thread.Sleep(1000);
         }
+
+        Console.WriteLine();
+        Console.WriteLine("Programa finalizado.");
     }
-
-    
-
 }
